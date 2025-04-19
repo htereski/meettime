0304d16be7991ca5ae7a17b7de415a5d78b5b4d1
htereski <tereski84@gmail.com>
2025-04-19 14:13:28 -0300

    initial commit

---
+++ src/main/java/com/meettime/meettime/MeettimeApplication.java
@@ -0,0 +1,13 @@
+package com.meettime.meettime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeettimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeettimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meettime/meettime/MeettimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meettime.meettime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeettimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
